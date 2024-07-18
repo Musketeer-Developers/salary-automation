84ee675fb3acbfac34297b3e2f1af5eacd647873
ahtishamdilawar <126797321+ahtishamdilawar@users.noreply.github.com>
2024-07-18 15:48:15 +0500

    added more basic collections

---
+++ src/api/daily-work/controllers/daily-work.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * daily-work controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::daily-work.daily-work');
---
+++ src/api/month-data/services/month-data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * month-data service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::month-data.month-data');
