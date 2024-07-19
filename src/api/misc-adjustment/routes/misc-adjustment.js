bd50a9621e79cd9bb4c4cce3cb027a7fab6cfb68
ahtishamdilawar <126797321+ahtishamdilawar@users.noreply.github.com>
2024-07-19 11:14:18 +0500

    basic collections

---
+++ src/api/misc-adjustment/routes/misc-adjustment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * misc-adjustment router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::misc-adjustment.misc-adjustment');
