780043b57a3c749ec22797da6350bbecae749e53
Moeez Muslim <mail2moeezmuslim@gmail.com>
2024-07-29 11:05:35 +0500

    Updated the env

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "salary-automation"),
       user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "12345678"),
+      password: env("DATABASE_PASSWORD", "admin"),
     },
     debug: false,
   },
